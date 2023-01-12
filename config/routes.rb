0c3fe1fdba9e300c0202f8bc9c151d6ae85cfbb1
ssss7277 <dreamsaki107@gmail.com>
2023-01-12 23:32:03 +0900

    add keyinput function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'game/play'
+  root to: 'game#play'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
