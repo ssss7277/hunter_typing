1b67e9ee80aac007fb7ba3c864b1f9134c8d467c
ssss7277 <dreamsaki107@gmail.com>
2023-01-19 16:20:51 +0900

    Screen transition for each mode

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -4,4 +4,19 @@ class GameController < ApplicationController
 
   def nomal_easy
   end
+
+  def nomal_easy
+  end
+
+  def nomal_hard
+  end
+
+  def rank_easy
+  end
+
+  def rank_medium
+  end
+
+  def rank_hard
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root to: 'game#play'
   get 'nomal_easy', to: 'game#nomal_easy'
+  get 'nomal_medium', to: 'game#nomal_meidum'
+  get 'nomal_hard', to: 'game#nomal_hard'
+  get 'rank_easy', to: 'game#rank_easy'
+  get 'rank_medium', to: 'game#rank_medium'
+  get 'rank_hard', to: 'game#rank_hard'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
